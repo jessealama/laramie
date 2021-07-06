183339c4845279f86ea1d971a251991adeadfccd
anonymous <Anon>
2021-07-06 05:33:18 +0000

    Add dependency on http-easy
    FossilOrigin-Name: e741309d17b647ae08864b67d9cf8af2707c9bfc2273fcf42242482ecb586c48
    
    FossilOrigin-Name: d731ca8ab40750df8d0ad34ffc56c8c7728c83504bbe861011acdc58d0d309e5

--- laramie-lib/info.rkt
+++ laramie-lib/info.rkt
@@ -6,6 +6,7 @@
 (define authors '("jesse@serverracket.com"))
 (define deps '("base"
                "typed-racket-lib"
-               "txexpr"))
+               "txexpr"
+               "http-easy"))
 (define build-deps '("rackunit-lib"
                      "rackunit-typed"))
