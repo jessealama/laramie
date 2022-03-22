7c7106f704b4019c14fe4b08652d25fd0b7f16c6
jesse <jesse@serverracket.com>
2022-03-22 05:18:20 +0000

    Switch to multi collection
    FossilOrigin-Name: 9936ed10f10470b784cb71c87ceef009469883943c504f6fbcd7335720cbc979

--- laramie-lib/info.rkt
+++ laramie-lib/info.rkt
@@ -1,9 +1,10 @@
 #lang info
 
-(define version "0.1")
-(define collection "laramie")
+(define version "0.2")
+(define collection 'multi)
 (define description "Implementation for Laramie")
 (define authors '("jesse@serverracket.com"))
+(define setup-collects '("laramie"))
 (define deps '("base"
                "typed-racket-lib"
                "txexpr"
