1877166001488a2faf512eb8cee3a213b7d544cb
jesse <jesse@serverracket.com>
2022-03-24 05:21:54 +0000

    Removed spurious info.rkt in root directory
    FossilOrigin-Name: adc150cc052580afd7bf347576f2a7bbbf7affc660cd91d42fb1de2a540b660b

--- laramie-lib/info.rkt
+++ laramie-lib/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
-(define version "0.2")
 (define collection "laramie")
+(define version "0.2")
 (define description "Implementation for Laramie")
 (define authors '("jesse@serverracket.com"))
 (define deps '("base"
