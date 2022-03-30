25b519afd4af49174b19481b8185ec0a46b4361b
jesse <jesse@serverracket.com>
2022-03-30 05:24:33 +0000

    Simplify tests (remove excessive data)
    FossilOrigin-Name: 76171ca2651619f916678b4590b8010f2ea579161a782b42ea0722400b80a737

--- laramie-test/laramie/issue-14.rkt
+++ laramie-test/laramie/issue-14.rkt
@@ -14,7 +14,7 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="foo" integrity="sha384" crossorigin="anonymous"></script>
   </body>
 </html>
 BOOTSTRAP
